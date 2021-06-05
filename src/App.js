479964551871d6381565ee45da08b25968fa7107
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2021-06-06 00:06:10 +0530

    finished styling MenuItem components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HomePage } from './pages/homepage/homepage.components.jsx'
 import './pages/homepage/homepage.styles.scss'
+import './App.css'
 
 function App() {
   return (
