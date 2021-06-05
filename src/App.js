f3d6d8d8366afb8d1c1449f204399f4c041e1a81
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2021-06-05 14:35:24 +0530

    created project and added homepage files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './homepage.component.jsx'
+import './homepage.styles.scss'
 
 function App() {
   return (
     <div >
-      {HomePage}
+        <HomePage/>
     </div>
   );
 }
