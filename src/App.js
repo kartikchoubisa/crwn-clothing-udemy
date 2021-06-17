43461f7a6bcab2128e6682e1f3d96ee4d67b3286
kartikchoubisa <f20190896@goa.bits-pilani.ac.in>
2021-06-17 15:57:03 +0530

    integrated firebase, created sign in page, and a sign out button, implemented sign in and sign out features

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { HomePage } from './pages/homepage/homepage.components.jsx'
-import {ShopPage} from "./pages/shop/shop.components";
+import { HomePage } from './pages/homepage/homepage.components.jsx';
+import {ShopPage} from "./pages/shop/shop.components.jsx";
+import Header from "./components/header/header.components.jsx";
+import SignInAndSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.components.jsx";
 import './App.css'
+import {auth} from './firebase/firebase.utils.js'
 
 import {Route, Switch} from "react-router-dom";
 
 
-function App() {
-  return (
-    <div >
-        <Switch>
-            <Route exact path = "/" component = {HomePage}/>
-            <Route exact path = "/shop" component = {ShopPage}/>
-        </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: null,
+        }
+    }
+
+    unsubscribeFromAuth = null
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+                this.setState({currentUser: user})
+                console.log(user)
+            }
+        )
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Header currentUser = {this.state.currentUser}/>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route exact path="/shop" component={ShopPage}/>
+                    <Route exact path="/signin" component={SignInAndSignUp}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
-
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './sign-in-sign-up.styles.scss';
+import SignIn from "../../components/sign-in/sign-in.components";
+
+const SignInSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn/>
+    </div>
+)
+
+export default SignInSignUpPage;
